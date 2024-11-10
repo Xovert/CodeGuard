9dc82098544bdd1e41a6b270709cb793072de64f
mandaania <amandanathania189@gmail.com>
2024-11-10 18:57:21 +0700

    landing page (index): done

--- imgwebapp/static/js/index.js
+++ imgwebapp/static/js/index.js
@@ -1,23 +1,30 @@
-document.getElementById("change-tab").addEventListener("click", function () {
-    var targetPaneId = this.getAttribute('data-target')
-    var targetTabId = this.getAttribute('tab-target')
-    var activeTab = document.querySelector('.tab-pane.active')
-    var activeButton = document.querySelector('.nav-link.active')
-    var targetTab = document.querySelector(targetTabId)
-    var targetPane = document.querySelector(targetPaneId)
+// SCROLL FUNCTION
+$(document).ready(function () {
+    // for all a tag
+    $("a").on('click', function (event) {
+        // if a tag is not empty
+        if (this.hash !== "") {
+            event.preventDefault();
 
-    document.querySelectorAll('.tab-pane').forEach(function(tab){
-        tab.classList.remove('active', 'show')
-    })
-
-    document.querySelectorAll('.nav-link').forEach(function(link){
-        link.classList.remove('active')
-    })
-
-    targetTab.classList.add('active')
-    targetPane.classList.add('show', 'active')
-})
-
-$(document).ready(function(){
-    $("#Error").modal('toggle');
+            // Store hash
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
+
+// for navbar showing and hiding
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-72px";
+  }
+  prevScrollpos = currentScrollPos;
+}
