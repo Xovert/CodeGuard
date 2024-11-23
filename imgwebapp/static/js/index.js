24730786167d87bb826c1915ba84bc7ef8ac23c5
21blackbird <115868293+21blackbird@users.noreply.github.com>
2024-11-23 21:54:29 +0700

    added change profile picture function at index.js, added change password at editprofile.html, added codeMirror6.js and codMirror6.min.js

--- imgwebapp/static/js/index.js
+++ imgwebapp/static/js/index.js
@@ -21,3 +21,16 @@ document.getElementById("change-tab").addEventListener("click", function () {
 $(document).ready(function(){
     $("#Error").modal('toggle');
 });
+
+function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const image = document.querySelector('.profile-picture');
+            image.src = e.target.result; // Update the image source
+        };
+        reader.readAsDataURL(file);
+    }
+}
+
