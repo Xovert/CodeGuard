210bed5cb8e55dbd3cacd6ddf3b3bb46dd66aaff
mandaania <amandanathania189@gmail.com>
2024-12-11 17:25:05 +0700

    add course page: add accordion

--- imgwebapp/static/js/admin_add_course.js
+++ imgwebapp/static/js/admin_add_course.js
@@ -1,3 +1,27 @@
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "flex") {
+            panel.style.display = "none";
+            this.style.paddingBottom = "0px"
+            this.style.borderBottom = "none"
+        } else {
+            panel.style.display = "flex";
+            this.style.paddingBottom = ""
+        }
+
+        // rotate icon
+        var dropdownIcon = this.querySelector(".dropdown-icon");
+        dropdownIcon.classList.toggle("rotate");
+    });
+}
+
+
+// edit options
 document.querySelectorAll('[contenteditable="true"]').forEach(label => {
     label.addEventListener('click', function (e) {
         e.preventDefault();
