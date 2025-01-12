2ac726326c3ad6ccee50ef6d911684a12bbc148d
Xovert <90762303+Xovert@users.noreply.github.com>
2025-01-13 03:20:12 +0700

    Added Contents Loader

--- CodeGuard/courses/catalogue.py
+++ CodeGuard/courses/catalogue.py
@@ -36,34 +36,6 @@ def get_enrolled():
         .join(Enrollments)
         .join(Users)
         .where(Users.uuid == user_uuid)
-        .order_by(Enrollments.last_accessed_time.asc())
+        .order_by(Enrollments.last_accessed_time.desc())
     ).all()
     return courses
-
-
-def update_time():
-    pass
-
-
-def get_modules(course_name):
-    user_uuid = get_uuid()
-    modules = db.session.execute(
-        db.select(Modules.module_name, Modules.order, EnrollmentsModules.progress)
-        .join(EnrollmentsModules, Modules.id == EnrollmentsModules.module_id)
-        .join(Enrollments, Enrollments.id == EnrollmentsModules.enrollment_id)
-        .join(Users, Users.id == Enrollments.user_id)
-        .join(Courses, Courses.id == Modules.course_id)
-        .where(Users.uuid == user_uuid)
-        .where(Courses.course_name == course_name)
-        .order_by(Modules.order.asc())
-    ).all()
-    return modules
-
-
-def get_course_fields(course_name):
-    course = db.session.execute(
-        db.select(Courses.course_name, Courses.description, CourseImages.new_filename)
-        .join(CourseImages)
-        .where(Courses.course_name == course_name)
-    ).first()
-    return course
---
+++ CodeGuard/courses/details.py
@@ -0,0 +1,67 @@
+from flask import session, current_app as app
+
+from CodeGuard.models import (
+    db, 
+    Courses, 
+    Enrollments,
+    Users,
+    CourseImages,
+    Modules,
+    EnrollmentsModules,
+)
+from CodeGuard.courses import courses
+from CodeGuard.utils.user import get_uuid
+from datetime import datetime, timezone, timedelta
+from urllib.parse import quote
+
+
+def update_time(id):
+    enrollment = db.session.scalars(
+        db.select(Enrollments)
+        .where(Enrollments.id == id)
+    ).first()
+    enrollment.last_accessed_time = datetime.now(tz=timezone(timedelta(hours=7)))
+    try:
+        db.session.commit()
+    except:
+        db.session.rolback()
+
+
+def get_enrollment_id(user_uuid, course_name):
+    enrollment_id = db.session.scalars(
+        db.select(Enrollments.id)
+        .join(Users)
+        .join(Courses)
+        .where(Users.uuid == user_uuid)
+        .where(Courses.course_name == course_name)
+    ).first()
+    return enrollment_id
+
+
+def get_modules(course_name):
+    user_uuid = get_uuid()
+    enrollment_id = get_enrollment_id(user_uuid, course_name)
+    if enrollment_id:
+        modules = db.session.execute(
+            db.select(Modules.module_name, Modules.order, EnrollmentsModules.progress)
+            .join(EnrollmentsModules, Modules.id == EnrollmentsModules.module_id)
+            .where(EnrollmentsModules.enrollment_id == enrollment_id)
+            .order_by(Modules.order.asc())
+        ).all()
+    else:
+        modules = db.session.execute(
+            db.select(Modules.module_name, Modules.order, 0)
+            .join(Courses)
+            .where(Courses.course_name == course_name)
+            .order_by(Modules.order.asc())
+        ).all()
+    return modules, enrollment_id
+
+
+def get_course_fields(course_name):
+    course = db.session.execute(
+        db.select(Courses.course_name, Courses.description, CourseImages.new_filename)
+        .join(CourseImages)
+        .where(Courses.course_name == course_name)
+    ).first()
+    return course
