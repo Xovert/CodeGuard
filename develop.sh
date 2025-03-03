81166df24ffba77e9b287e8bb6211421ffff9b71
Xovert <90762303+Xovert@users.noreply.github.com>
2025-03-03 18:00:58 +0700

    Entrypoint modification

--- develop.sh
+++ develop.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -f 'entrypoint.sh' ]; then
+  cp docker-entrypoint.sh entrypoint.sh
+fi
+
 if [ ! -f 'CodeGuard/config.ini' ]; then
   echo "An error has occured!"
   echo "You must first create a config.ini file."
